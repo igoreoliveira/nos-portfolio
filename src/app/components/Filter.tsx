749d9d26e935751e25429c8e077018bfe789468b
Eliabe Castro <eliabe.castro@omeletecompany.com>
2023-11-03 20:04:12 -0300

    wip

--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -1,5 +1,35 @@
 "use client";
 
-export default function Header() {
-  return "vvvvvvvvvvvvv";
+import { Chip } from "@mui/material";
+
+export default function Filter() {
+  return (
+    <>
+      <Chip
+        label={"Todos"}
+        variant="outlined"
+        onClick={() => console.log("click")}
+      />
+      <Chip
+        label={"Desenvolvimento de Jogos"}
+        variant="outlined"
+        onClick={() => console.log("click")}
+      />
+      <Chip
+        label={"Design de Jogos"}
+        variant="outlined"
+        onClick={() => console.log("click")}
+      />
+      <Chip
+        label={"Computação Gráfica"}
+        variant="outlined"
+        onClick={() => console.log("click")}
+      />
+      <Chip
+        label={"Inovação"}
+        variant="outlined"
+        onClick={() => console.log("click")}
+      />
+    </>
+  );
 }
