46094cbaf2fcf85454ae8824ec0973d4b5c136f3
Eliabe Castro <eliabe.castro@omeletecompany.com>
2023-11-03 21:06:48 -0300

    add filter

--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -2,33 +2,50 @@
 
 import { Chip } from "@mui/material";
 
-export default function Filter() {
+export default function Filter({ subjects, updateSubjects }) {
+  const handleClick = (event) => {
+    const selectedSubject = event.target.innerText;
+    if (selectedSubject === "Todos") {
+      updateSubjects([]);
+      return;
+    }
+    if (subjects.includes(selectedSubject)) {
+      updateSubjects(subjects.filter((sub) => sub !== selectedSubject));
+    } else {
+      updateSubjects([...subjects, selectedSubject]);
+    }
+  };
+
   return (
     <>
       <Chip
         label={"Todos"}
-        variant="outlined"
-        onClick={() => console.log("click")}
+        variant={subjects.length < 1 ? "filled" : "outlined"}
+        onClick={handleClick}
       />
       <Chip
         label={"Desenvolvimento de Jogos"}
-        variant="outlined"
-        onClick={() => console.log("click")}
+        variant={
+          subjects.includes("Desenvolvimento de Jogos") ? "filled" : "outlined"
+        }
+        onClick={handleClick}
       />
       <Chip
         label={"Design de Jogos"}
-        variant="outlined"
-        onClick={() => console.log("click")}
+        variant={subjects.includes("Design de Jogos") ? "filled" : "outlined"}
+        onClick={handleClick}
       />
       <Chip
         label={"Computação Gráfica"}
-        variant="outlined"
-        onClick={() => console.log("click")}
+        variant={
+          subjects.includes("Computação Gráfica") ? "filled" : "outlined"
+        }
+        onClick={handleClick}
       />
       <Chip
         label={"Inovação"}
-        variant="outlined"
-        onClick={() => console.log("click")}
+        variant={subjects.includes("Inovação") ? "filled" : "outlined"}
+        onClick={handleClick}
       />
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
+"use client";
+
 import ProjectSection from "./components/Projects";
 import About from "./components/About";
 import Header from "./components/Header";
 import Filter from "./components/Filter";
 import { projects } from "./mock";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedSubjects, setSelectedSubjects] = useState([]);
+  const updateSubjects = (updatedSubjects: []) => {
+    setSelectedSubjects(updatedSubjects);
+  };
+
+  const activeProjects =
+    selectedSubjects.length > 0
+      ? projects.filter((project) =>
+          project.relatedFields.some((r) => selectedSubjects.includes(r))
+        )
+      : projects;
+
   return (
     <div>
       <Header />
       <About />
-      <Filter />
+      <Filter subjects={selectedSubjects} updateSubjects={updateSubjects} />
       <div>
-        {projects.map((project) => (
+        {activeProjects.map((project) => (
           <ProjectSection project={project} />
         ))}
       </div>
